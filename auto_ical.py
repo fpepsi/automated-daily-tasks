4e6b23747b98c38b401ee83855da20a6e8cc4e57
fpepsi <13fpepsi@gmail.com>
2025-03-12 18:30:30 -0400

    Update auto_ical.py
    I accidentally added private information that I needed to remove

--- auto_ical.py
+++ auto_ical.py
@@ -1,6 +1,6 @@
 import subprocess
 
-applescript_path = "/Users/fabiobethlem/Documents/Work/Courses/Udemy_100_Days_Python/capstone_projects/automated-daily-tasks/Calendar Download.scpt"
+applescript_path = "/Calendar Download.scpt"
 
 try:
     result = subprocess.run(["osascript", applescript_path], capture_output=True, text=True, check=True)
